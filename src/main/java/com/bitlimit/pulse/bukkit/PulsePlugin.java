b993b74cce44b06cf48a71983f145cc6392891f5
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-10-21 23:11:24 -0700

    Import code

---
+++ src/main/java/com/bitlimit/pulse/bukkit/PulsePlugin.java
@@ -0,0 +1,18 @@
+package com.bitlimit.bits.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PulsePlugin extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+
+	}
+
+	@Override
+	public void onDisable()
+	{
+
+	}
+}
